764dfe779e286d40510a5925e795bf2dfcf4081c
Henrik Karlsson <henrik.axel.karlsson@gmail.com>
2018-10-14 22:27:33 +0300

    Initial commit

---
+++ init.el
@@ -0,0 +1,20 @@
+(package-initialize)
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(package-initialize)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (manoj-dark))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(prefer-coding-system 'utf-8)
+(setq explicit-shell-file-name "C:/Program Files/Git/bin/bash.exe")
+(setq explicit-bash.exe-args '("--login" "-i"))
