8b9516b7beec7e008ebdb3dea618cfc057b6932a
Henrik Karlsson <henrik.axel.karlsson@gmail.com>
2018-10-16 09:40:48 +0300

    Set UTF-8 encoding to be the default

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # .emacs.d
+
+Make a .emacs file in ~/AppData/Roaming/ and insert the following into it:
+
+(setq user-init-file "C:/Users/henrik.karlsson/.emacs.d/init.el")
+(setq user-emacs-directory "C:/Users/henrik.karlsson/.emacs.d")
+(setq default-directory "C:/Users/henrik.karlsson/")
+(setenv "HOME" "C:/Users/henrik.karlsson/")
+(load user-init-file)
+
+This will set the home directories to be more in line with what you'd expect, and you can have your actual emacs initialization files in ~/.emacs.d/
--- init.el
+++ init.el
@@ -15,6 +15,20 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(setq inhibit-startup-message t)
+(setq-default buffer-file-coding-system 'utf-8-unix)
+(set-terminal-coding-system 'utf-8)
+(set-language-environment 'utf-8)
+(set-keyboard-coding-system 'utf-8)
 (prefer-coding-system 'utf-8)
-(setq explicit-shell-file-name "C:/Program Files/Git/bin/bash.exe")
-(setq explicit-bash.exe-args '("--login" "-i"))
+(setq locale-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+
+(if (equal system-type 'windows-nt)
+    (progn (setq explicit-shell-file "C:/Users/henrik.karlsson/AppData/Local/Programs/Git/bin/sh.exe")
+	   (setq shell-file-name "bash")
+	   (setq explicit-sh.exe-args '("--login" "-i"))
+	   (setenv "SHELL" shell-file-name)
+	   (add-hook 'comint-output-filter-functions 'comint-strip-ctrl-m)))
