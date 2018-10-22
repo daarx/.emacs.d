71d791f8ef80849a0115c6808b4e08dfc0eec59d
Henrik Karlsson <henrik.axel.karlsson@gmail.com>
2018-10-22 17:22:05 +0300

    small changes to task.org and renamed test.org to test.txt

--- init.el
+++ init.el
@@ -66,3 +66,6 @@
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cc" 'org-capture)
 (global-set-key "\C-cb" 'org-switchb)
+
+(setq org-clock-persist 'history)
+(org-clock-persistence-insinuate)
